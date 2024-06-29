02c699d86f91f8c4aa6b5f505384652a63ff2531
Hugo <valladahugo@gmail.com>
2024-06-29 19:34:39 -0300

    Adiciona exemplo para ouvir mensagens

--- main_test.go
+++ main_test.go
@@ -14,6 +14,12 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
+const (
+	REGION       string = "us-east-1"
+	ENDPOINT_ENV string = "AWS_ENDPOINT"
+	QUEUE_NAME          = "notificacao-sqs"
+)
+
 func TestLocalContainer(t *testing.T) {
 	ctx := context.Background()
 	req := testcontainers.ContainerRequest{
@@ -22,7 +28,7 @@ func TestLocalContainer(t *testing.T) {
 		WaitingFor:   wait.ForLog("Ready."),
 		Env: map[string]string{
 			"SERVICES":              "sqs",
-			"DEFAULT_REGION":        "us-east-1",
+			"DEFAULT_REGION":        REGION,
 			"AWS_ACCESS_KEY_ID":     "test",
 			"AWS_SECRET_ACCESS_KEY": "test",
 		},
@@ -36,37 +42,42 @@ func TestLocalContainer(t *testing.T) {
 	}
 	defer localstackContainer.Terminate(ctx)
 
-	sdkConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-east-1"))
+	sdkConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion(REGION))
 	if err != nil {
 		log.Println("Não foi possível carregar a configuração padrão. Você configurou suas credenciais AWS?")
 		log.Fatal(err)
 	}
 	sqsClient := sqs.NewFromConfig(sdkConfig)
 	sqsClient.Options().EndpointResolverV2.ResolveEndpoint(ctx, sqs.EndpointParameters{
-		Region:   aws.String("us-east-1"),
+		Region:   aws.String(REGION),
 		Endpoint: aws.String("http://localhost:4566"),
 	})
-	queueName := "minha-fila-sqs"
 	returnQueue, err := sqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
-		QueueName: &queueName,
+		QueueName: aws.String(QUEUE_NAME),
 	})
 	fmt.Println(*returnQueue.QueueUrl)
-	t.Setenv("AWS_ENDPOINT", *returnQueue.QueueUrl)
+	t.Setenv(ENDPOINT_ENV, *returnQueue.QueueUrl)
 	if err != nil {
 		log.Printf("Não foi possível criar a fila: %v\n", err)
 		return
 	}
 
 	fmt.Println("Fila criada com sucesso!")
-	fmt.Println(os.Getenv("AWS_ENDPOINT"))
+	fmt.Println(os.Getenv(ENDPOINT_ENV))
 	_, err = sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
 		MessageBody: aws.String("Ola"),
-		QueueUrl:    aws.String(os.Getenv("AWS_ENDPOINT")),
+		QueueUrl:    aws.String(os.Getenv(ENDPOINT_ENV)),
 	})
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println("Mensagem enviada com sucesso")
 	}
+	msg, err := sqsClient.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(os.Getenv(ENDPOINT_ENV)),
+		MaxNumberOfMessages: *aws.Int32(2),
+	})
+	for _, msg := range msg.Messages {
+		fmt.Println(*msg.Body)
+	}
 }
-
