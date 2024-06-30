a7aa67d46cc5742f9566b256e1aa2a03a724dc39
Hugo <valladahugo@gmail.com>
2024-06-29 23:27:31 -0300

    Adiciona exemplo para criar um testcontainer para todos os testes

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module example
 go 1.22.4
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.1
 	github.com/aws/aws-sdk-go-v2/config v1.27.23
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.34.1
+	github.com/stretchr/testify v1.9.0
 	github.com/testcontainers/testcontainers-go v0.31.0
 )
 
@@ -13,7 +15,6 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/Microsoft/hcsshim v0.11.4 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.30.1 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.23 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.9 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.13 // indirect
@@ -29,6 +30,7 @@ require (
 	github.com/containerd/containerd v1.7.15 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/cpuguy83/dockercfg v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/distribution/reference v0.5.0 // indirect
 	github.com/docker/docker v25.0.5+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
@@ -41,6 +43,7 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
@@ -51,6 +54,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
@@ -69,4 +73,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230731190214-cbb8c96f2d6d // indirect
 	google.golang.org/grpc v1.58.3 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
@@ -19,6 +20,75 @@ const (
 	ENDPOINT_ENV string = "AWS_ENDPOINT"
 	QUEUE_NAME          = "notificacao-sqs"
 )
+
+func TestAll(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "localstack/localstack",
+		ExposedPorts: []string{"4566/tcp"},
+		WaitingFor:   wait.ForLog("Ready."),
+		Env: map[string]string{
+			"SERVICES":              "sqs",
+			"DEFAULT_REGION":        REGION,
+			"AWS_ACCESS_KEY_ID":     "test",
+			"AWS_SECRET_ACCESS_KEY": "test",
+		},
+	}
+	localstackContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer localstackContainer.Terminate(ctx)
+	sqsClient := prepareSqsClient()
+	returnQueue, err := sqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
+		QueueName: aws.String(QUEUE_NAME),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(*returnQueue.QueueUrl)
+	t.Setenv(ENDPOINT_ENV, *returnQueue.QueueUrl)
+	t.Run("Sucess", LocalContainerSimplified)
+	t.Run("Error", LocalContainerErrorSimplified)
+}
+
+func prepareSqsClient() sqs.Client {
+	ctx := context.Background()
+	sdkConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion(REGION))
+	if err != nil {
+		log.Println("Não foi possível carregar a configuração padrão. Você configurou suas credenciais AWS?")
+		log.Fatal(err)
+	}
+	sqsClient := sqs.NewFromConfig(sdkConfig)
+	sqsClient.Options().EndpointResolverV2.ResolveEndpoint(ctx, sqs.EndpointParameters{
+		Region:   aws.String(REGION),
+		Endpoint: aws.String("http://localhost:4566"),
+	})
+	return *sqsClient
+}
+
+func LocalContainerSimplified(t *testing.T) {
+	ctx := context.Background()
+	sqsClient := prepareSqsClient()
+	_, err := sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		MessageBody: aws.String("Ola"),
+		QueueUrl:    aws.String(os.Getenv(ENDPOINT_ENV)),
+	})
+	assert.NoError(t, err)
+}
+
+func LocalContainerErrorSimplified(t *testing.T) {
+	ctx := context.Background()
+	sqsClient := prepareSqsClient()
+	_, err := sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		MessageBody: aws.String("Ola"),
+		QueueUrl:    aws.String("http:"),
+	})
+	assert.Error(t, err)
+}
 
 func TestLocalContainer(t *testing.T) {
 	ctx := context.Background()
@@ -83,4 +153,60 @@ func TestLocalContainer(t *testing.T) {
 	for _, msg := range msg.Messages {
 		fmt.Println(*msg.Body)
 	}
+	assert.NoError(t, err)
+}
+
+func TestLocalContainerError(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "localstack/localstack",
+		ExposedPorts: []string{"4566/tcp"},
+		WaitingFor:   wait.ForLog("Ready."),
+		Env: map[string]string{
+			"SERVICES":              "sqs",
+			"DEFAULT_REGION":        REGION,
+			"AWS_ACCESS_KEY_ID":     "test",
+			"AWS_SECRET_ACCESS_KEY": "test",
+		},
+	}
+	localstackContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer localstackContainer.Terminate(ctx)
+
+	sdkConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion(REGION))
+	if err != nil {
+		log.Println("Não foi possível carregar a configuração padrão. Você configurou suas credenciais AWS?")
+		log.Fatal(err)
+	}
+	sqsClient := sqs.NewFromConfig(sdkConfig)
+	sqsClient.Options().EndpointResolverV2.ResolveEndpoint(ctx, sqs.EndpointParameters{
+		Region:   aws.String(REGION),
+		Endpoint: aws.String("http://localhost:4566"),
+	})
+	returnQueue, err := sqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
+		QueueName: aws.String(QUEUE_NAME),
+	})
+	fmt.Println(*returnQueue.QueueUrl)
+	if err != nil {
+		log.Printf("Não foi possível criar a fila: %v\n", err)
+		return
+	}
+
+	fmt.Println("Fila criada com sucesso!")
+	fmt.Println(os.Getenv(ENDPOINT_ENV))
+	_, err = sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		MessageBody: aws.String("Ola"),
+		QueueUrl:    aws.String(os.Getenv(ENDPOINT_ENV)),
+	})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Mensagem enviada com sucesso")
+	}
+	assert.Error(t, err)
 }
